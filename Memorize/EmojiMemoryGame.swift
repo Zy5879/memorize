9c049d36c10359cc7181befb15cd64d39c7a018e
Zy5879 <ZaireM21@gmail.com>
2023-09-26 16:41:06 -0400

    MVVM Seperation

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Zaire McAllister on 9/26/23.
+//
+
+import Foundation
---
+++ Memorize/MemorizeGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Zaire McAllister on 9/26/23.
+//
+
+import Foundation
